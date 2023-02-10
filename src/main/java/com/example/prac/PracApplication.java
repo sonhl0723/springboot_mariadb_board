bf328eb02ffafa65ca60ce1bdf94a5573703d314
sonhl0723 <sonhl0723@naver.com>
2023-02-10 10:38:53 +0900

    chore: Env setting
    mariadb-java-client
    mybatis-spring-boot-starter
    spring-boot-devtools
    springfox-swagger2 / springfox-swagger-ui
    tomcat-embed-jasper
    jstl

--- src/main/java/com/example/prac/PracApplication.java
+++ src/main/java/com/example/prac/PracApplication.java
@@ -3,7 +3,10 @@ package com.example.prac;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class PracApplication {
 
 	public static void main(String[] args) {
